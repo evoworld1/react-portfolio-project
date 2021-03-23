5ae0bfcf4797c7f8867f4fc56e315f4dc6594e54
evoworld1 <evoworld1@gmail.com>
2021-03-23 11:32:50 +0100

    Project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>let's begin</h1>
+    </div>
+  );
+}
+
+export default App;
