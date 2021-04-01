a5495165a6a4537ba39e56dcabb8dc9e4c993979
evoworld1 <evoworld1@gmail.com>
2021-04-01 22:09:54 +0200

    Add contact form, update css

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-container">
+      <form action="https://formsubmit.co/evoworld1@gmail.com" method="POST">
+        <div className="form-group">
+          <label for="inputEmailAdress">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="inputEmailAdress"
+            aria-describedby="emailHelp"
+            placeholder="Enter your email"
+            name="email"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label for="inputSubject">Subject</label>
+          <input
+            type="subject"
+            className="form-control"
+            id="inputSubject"
+            name="subject"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="formTextarea">Message</label>
+          <textarea
+            class="form-control"
+            name="message"
+            id="formTextarea"
+            rows="4"
+          ></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
